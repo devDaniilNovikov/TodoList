86e059e012db8e00fa6f40d2d057e0e451109657
danilanovikov <novikovdanila7@gmail.com>
2025-04-26 12:51:04 +0300

    add RabbitMq

--- src/main/java/dn/tasktracker/configuration/RabbitConfig.java
+++ src/main/java/dn/tasktracker/configuration/RabbitConfig.java
@@ -1,8 +1,14 @@
 package dn.tasktracker.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
@@ -12,6 +18,34 @@ import org.springframework.context.annotation.Configuration;
 
 public class RabbitConfig {
 
-    private final RabbitProperties rabbitProperties;
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Value("${}spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${messaging.queue.name}")
+    private String queueName;
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMandatory(true);
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Queue queue() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host, port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        return new Queue(queueName,true);
+    }
 
 }
